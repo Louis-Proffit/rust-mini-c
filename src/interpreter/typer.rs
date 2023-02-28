65448e46c38c632b78e33ce1cab78bd589ee8f87
LouisProffitX <louisproffit86@gmail.com>
2023-02-28 14:12:37 +0100

    Add errs to typer interpreter

--- src/interpreter/typer.rs
+++ src/interpreter/typer.rs
@@ -5,12 +5,13 @@ use std::rc::Rc;
 use derive_new::new;
 use derive_getters::Getters;
 use crate::interpreter::Stdout;
+use crate::interpreter::typer::bool::{Bool, ToCBool};
 use crate::interpreter::typer::defaults::{Malloc, Putchar};
 use crate::interpreter::typer::error::TypInterpreterError;
 use crate::typer::structure::{Binop, Block, BlockIdent, Expr, ExprNode, File, Fun, Ident, Stmt, Unop};
 
 
-pub type InterpreterResult = Result<Stdout, TypInterpreterError>;
+pub type InterpreterResult<T> = Result<T, TypInterpreterError>;
 
 const DEFAULT_RETURN_VALUE: Value = 0;
 
@@ -22,7 +23,7 @@ struct Context<'a> {
     stdout: Rc<RefCell<Stdout>>,
 }
 
-pub fn interp_file<'a>(file: File<'a>) -> InterpreterResult {
+pub fn interp_file<'a>(file: File<'a>) -> InterpreterResult<Stdout> {
     let vars = MemoryVar::new();
     let memory = Rc::new(RefCell::new(HashMap::new()));
     let stdout = Rc::new(RefCell::new(Stdout::new()));
@@ -44,82 +45,82 @@ pub fn interp_file<'a>(file: File<'a>) -> InterpreterResult {
         stdout.clone(),
     );
 
-    functions.get("main").expect("No main function").call(&mut context);
+    let _ = functions.get("main").expect("No main function").call(&mut context)?;
 
     let output = stdout.borrow().clone();
     Ok(output)
 }
 
-fn interp_stmt<'a>(context: &mut Context<'a>, stmt: &Stmt<'a>) -> Option<Value> {
+fn interp_stmt<'a>(context: &mut Context<'a>, stmt: &Stmt<'a>) -> InterpreterResult<Option<Value>> {
     match stmt {
-        Stmt::SSkip => None,
+        Stmt::SSkip => Ok(None),
         Stmt::SExpr(e) => {
-            interp_expr(context, e);
+            let _ = interp_expr(context, e)?;
 
-            None
+            Ok(None)
         }
         Stmt::SIf(expr, stmt_if, stmt_else) => {
-            if let Some(x) = if interp_expr(context, expr).bool() {
-                interp_stmt(context, stmt_if)
+            if let Some(x) = if interp_expr(context, expr)?.bool() {
+                interp_stmt(context, stmt_if)?
             } else {
-                interp_stmt(context, stmt_else)
+                interp_stmt(context, stmt_else)?
             } {
-                return Some(x);
+                return Ok(Some(x));
             }
 
-            None
+            Ok(None)
         }
         Stmt::SWhile(expr, stmt) => {
-            while interp_expr(context, expr).bool() {
-                if let Some(x) = interp_stmt(context, stmt) {
-                    return Some(x);
+            while interp_expr(context, expr)?.bool() {
+                if let Some(x) = interp_stmt(context, stmt)? {
+                    return Ok(Some(x));
                 }
             }
-            None
+            Ok(None)
         }
         Stmt::SBlock(block) => {
             interp_block(context, block)
         }
         Stmt::SReturn(expr) => {
-            Some(interp_expr(context, expr))
+            Ok(Some(interp_expr(context, expr)?))
         }
     }
 }
 
-fn interp_block<'a>(context: &mut Context<'a>, block: &Block<'a>) -> Option<Value> {
+fn interp_block<'a>(context: &mut Context<'a>, block: &Block<'a>) -> InterpreterResult<Option<Value>> {
     for stmt in block.stmts() {
-        if let Some(x) = interp_stmt(context, stmt) {
-            return Some(x);
+        if let Some(x) = interp_stmt(context, stmt)? {
+            return Ok(Some(x));
         }
     }
 
-    None
+    Ok(None)
 }
 
-fn interp_expr<'a>(context: &mut Context<'a>, expr: &Expr<'a>) -> Value {
+fn interp_expr<'a>(context: &mut Context<'a>, expr: &Expr<'a>) -> InterpreterResult<Value> {
     match expr.node() {
-        ExprNode::EConst(x) => *x as Value,
+        ExprNode::EConst(x) => Ok(*x as Value),
         ExprNode::EAccessLocal(x) => {
-            context.vars.get(x.clone())
+            Ok(context.vars.get(x.clone()))
         }
         ExprNode::EAccessField(expr, y) => {
-            let address = interp_expr(context, expr);
-            context.memory
+            let address = interp_expr(context, expr)?;
+            Ok(context.memory
                 .borrow()
                 .get(&address)
                 .expect("Address doesn't point to anything")
                 .borrow_mut()
-                .get(y.name().clone())
+                .get(y.name().clone()))
         }
         ExprNode::EAssignLocal(var, expr) => {
-            let value = interp_expr(context, expr);
+            let value = interp_expr(context, expr)?;
             context.vars.set(var.clone(), value);
-            value
+            Ok(value)
         }
         ExprNode::EAssignField(expr, field, value) => {
-            let value = interp_expr(context, value);
+            let value = interp_expr(context, value)?;
 
-            let address = interp_expr(context, expr);
+            let address = interp_expr(context, expr)?;
 
             context.memory
                 .borrow_mut()
@@ -128,33 +129,35 @@ fn interp_expr<'a>(context: &mut Context<'a>, expr: &Expr<'a>) -> Value {
                 .borrow_mut()
                 .set(field.name().clone(), value);
 
-            value
+            Ok(value)
         }
-        ExprNode::EUnop(unop, expr) => match unop {
+        ExprNode::EUnop(unop, expr) => Ok(match unop {
             Unop::UNot => {
-                if interp_expr(context, expr).bool() { 0 } else { 1 }
+                if interp_expr(context, expr)?.bool() { 0 } else { 1 }
             }
-            Unop::UMinus => -interp_expr(context, expr)
-        }
-        ExprNode::EBinop(binop, expr_1, expr_2) => match binop {
-            Binop::BEq => (interp_expr(context, expr_1) == interp_expr(context, expr_2)).to_c_bool(),
-            Binop::BNeq => (interp_expr(context, expr_1) != interp_expr(context, expr_2)).to_c_bool(),
-            Binop::BLt => (interp_expr(context, expr_1) < interp_expr(context, expr_2)).to_c_bool(),
-            Binop::BGt => (interp_expr(context, expr_1) > interp_expr(context, expr_2)).to_c_bool(),
-            Binop::BGe => (interp_expr(context, expr_1) >= interp_expr(context, expr_2)).to_c_bool(),
-            Binop::BLe => (interp_expr(context, expr_1) <= interp_expr(context, expr_2)).to_c_bool(),
-            Binop::BAnd => (interp_expr(context, expr_1).bool() && interp_expr(context, expr_2).bool()).to_c_bool(),
-            Binop::BOr => (interp_expr(context, expr_1).bool() || interp_expr(context, expr_2).bool()).to_c_bool(),
-            Binop::BAdd => interp_expr(context, expr_1) + interp_expr(context, expr_2),
-            Binop::BSub => interp_expr(context, expr_1) - interp_expr(context, expr_2),
-            Binop::BMul => interp_expr(context, expr_1) * interp_expr(context, expr_2),
-            Binop::BDiv => interp_expr(context, expr_1) / interp_expr(context, expr_2),
-        }
+            Unop::UMinus => -interp_expr(context, expr)?
+        }),
+        ExprNode::EBinop(binop, expr_1, expr_2) => Ok(
+            match binop {
+                Binop::BEq => (interp_expr(context, expr_1)? == interp_expr(context, expr_2)?).to_c_bool(),
+                Binop::BNeq => (interp_expr(context, expr_1)? != interp_expr(context, expr_2)?).to_c_bool(),
+                Binop::BLt => (interp_expr(context, expr_1)? < interp_expr(context, expr_2)?).to_c_bool(),
+                Binop::BGt => (interp_expr(context, expr_1)? > interp_expr(context, expr_2)?).to_c_bool(),
+                Binop::BGe => (interp_expr(context, expr_1)? >= interp_expr(context, expr_2)?).to_c_bool(),
+                Binop::BLe => (interp_expr(context, expr_1)? <= interp_expr(context, expr_2)?).to_c_bool(),
+                Binop::BAnd => (interp_expr(context, expr_1)?.bool() && interp_expr(context, expr_2)?.bool()).to_c_bool(),
+                Binop::BOr => (interp_expr(context, expr_1)?.bool() || interp_expr(context, expr_2)?.bool()).to_c_bool(),
+                Binop::BAdd => interp_expr(context, expr_1)? + interp_expr(context, expr_2)?,
+                Binop::BSub => interp_expr(context, expr_1)? - interp_expr(context, expr_2)?,
+                Binop::BMul => interp_expr(context, expr_1)? * interp_expr(context, expr_2)?,
+                Binop::BDiv => interp_expr(context, expr_1)? / interp_expr(context, expr_2)?,
+            }
+        ),
         ExprNode::ECall(fun, args) => {
             let mut vars = MemoryVar::new();
 
             for (formal, arg) in zip(fun.args(), args) {
-                vars.set((formal.name(), 0), interp_expr(context, arg));
+                vars.set((formal.name(), 0), interp_expr(context, arg)?);
             }
 
             let mut new_context = Context::new(
@@ -164,13 +167,13 @@ fn interp_expr<'a>(context: &mut Context<'a>, expr: &Expr<'a>) -> Value {
                 context.stdout.clone(),
             );
 
-            context.functions
+            Ok(context.functions
                 .get(fun.profile().name())
                 .expect("Function doesn't exist")
-                .call(&mut new_context)
-                .unwrap_or(DEFAULT_RETURN_VALUE)
+                .call(&mut new_context)?
+                .unwrap_or(DEFAULT_RETURN_VALUE))
         }
-        ExprNode::ESizeof(x) => x.c_size() as Value
+        ExprNode::ESizeof(x) => Ok(x.c_size() as Value)
     }
 }
 
@@ -225,41 +228,45 @@ impl<'x> MemoryStruct<'x> {
     }
 }
 
-trait Bool {
-    fn bool(&self) -> bool;
-}
-
-impl Bool for Value {
-    fn bool(&self) -> bool {
-        *self != 0
-    }
-}
-
-trait ToCBool {
-    fn to_c_bool(&self) -> Value;
-}
-
-impl ToCBool for bool {
-    fn to_c_bool(&self) -> Value {
-        if *self { 1 } else { 0 }
-    }
+trait InterpreterCallable<'a> {
+    fn call(&self, context: &mut Context<'a>) -> InterpreterResult<Option<Value>>;
 }
 
 impl<'a> InterpreterCallable<'a> for Fun<'a> {
-    fn call(&self, context: &mut Context<'a>) -> Option<Value> {
+    fn call(&self, context: &mut Context<'a>) -> InterpreterResult<Option<Value>> {
         interp_block(context, self.block())
     }
 }
 
-trait InterpreterCallable<'a> {
-    fn call(&self, context: &mut Context<'a>) -> Option<Value>;
+mod bool {
+    use crate::interpreter::typer::Value;
+
+    pub trait Bool {
+        fn bool(&self) -> bool;
+    }
+
+    impl Bool for Value {
+        fn bool(&self) -> bool {
+            *self != 0
+        }
+    }
+
+    pub trait ToCBool {
+        fn to_c_bool(&self) -> Value;
+    }
+
+    impl ToCBool for bool {
+        fn to_c_bool(&self) -> Value {
+            if *self { 1 } else { 0 }
+        }
+    }
 }
 
 pub mod defaults {
     use std::cell::RefCell;
     use std::sync::Mutex;
     use derive_new::new;
-    use crate::interpreter::typer::{Context, InterpreterCallable, MemoryStruct, Value};
+    use crate::interpreter::typer::{Context, InterpreterCallable, InterpreterResult, MemoryStruct, Value};
 
     static MEMORY_INDEX: Mutex<i64> = Mutex::new(1);
 
@@ -270,16 +277,16 @@ pub mod defaults {
     pub struct Putchar {}
 
     impl<'a> InterpreterCallable<'a> for Putchar {
-        fn call(&self, context: &mut Context<'a>) -> Option<Value> {
+        fn call(&self, context: &mut Context<'a>) -> InterpreterResult<Option<Value>> {
             let value = context.vars.get(("c", 0)); // TODO arg;
             context.stdout.borrow_mut().putchar(value as u8);
 
-            Some(value)
+            Ok(Some(value))
         }
     }
 
     impl<'a> InterpreterCallable<'a> for Malloc {
-        fn call(&self, context: &mut Context<'a>) -> Option<Value> {
+        fn call(&self, context: &mut Context<'a>) -> InterpreterResult<Option<Value>> {
             let mut index = MEMORY_INDEX.lock().unwrap();
 
             context.memory.borrow_mut().insert(*index, RefCell::new(MemoryStruct::new()));
@@ -287,7 +294,7 @@ pub mod defaults {
             let old_index = index.clone();
             *index += 1;
 
-            Some(old_index)
+            Ok(Some(old_index))
         }
     }
 }
